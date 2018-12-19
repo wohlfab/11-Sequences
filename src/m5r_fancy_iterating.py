47d9399fd45407f6059e474c9e6f2e2c131c81f8
wohlfab <wohlfab@rose-hulman.edu>
2018-12-19 14:22:20 -0500

    Classwork 11

--- src/m5r_fancy_iterating.py
+++ src/m5r_fancy_iterating.py
@@ -13,12 +13,12 @@ is different from:
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Brandon Wohlfarth.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 ###############################################################################
-# TODO: 2. READ the program below and RUN it.
+# DONE: 2. READ the program below and RUN it.
 #
 #   When you have read it, asking questions as needed,
 #   and you feel that you understand:
